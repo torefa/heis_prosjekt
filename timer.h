bd121d4ed4652460906a0c4d2c5375f249251b45
torefa <torfad@gmail.com>
2017-02-15 18:42:47 +0100

    Update timer.h

--- timer.h
+++ timer.h
@@ -1,1 +1,15 @@
+//Header for the timer class
+//
+#include <stdio.h>
+#include <unistd.h>
 
+// Function to start the timer.
+void timer_start(){
+  sleep(3);
+  time_out(0);
+}
+
+// Funtion to notify that the time is out
+int time_out(int time){
+  return time;
+}
