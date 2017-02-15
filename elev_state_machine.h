cb8309e845bd92c47225a3be2b7dfe5c875806b1
torefa <torfad@gmail.com>
2017-02-15 19:46:31 +0100

    Update elev_state_machine.h

--- elev_state_machine.h
+++ elev_state_machine.h
@@ -2,13 +2,13 @@
 #pragma once
 
 // Event button is pressed
-void evButtonPressed();
+void evButtonPressed(void);
 
 // Event floor is reached
-void evFloor_reached();
+void evFloor_reached(void);
 
 // Event timer has timed out
-void evTime_out();
+void evTime_out(void);
 
 // Event Stop button is pressed
-void evStop_button_signal();
+void evStop_button_signal(void);
