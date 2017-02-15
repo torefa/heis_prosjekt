0fb4fa28dfdce586283d40848688c64a01526547
Magnus Danielsen <magnudan@stud.ntnu.no>
2017-02-15 18:58:04 +0100

    HW updatde
    Added function declarations

--- hw.c
+++ hw.c
@@ -1,1 +1,29 @@
-//Testing
+#include<stdlib.h>
+#include "io.h"
+#include ""
+
+int get_sensor_status(){
+	
+}
+
+int get_button_status(){
+	
+}
+
+void set_floor_status(nt button){
+	
+}
+
+void set_button_lamp(int lamp){
+	
+}
+
+void run_motor(int motor_dir){
+	
+}
+
+void stop_motor(int current_dir){
+	elev_set_motor_direction(- current_dir); 		// Reverses direction of the motor for 0.1 seconds
+	delay(100);
+	elev_set_motor_durection(0); 						// 
+};
--- hw.h
+++ hw.h
@@ -1,1 +1,11 @@
+int get_sensor_status();
 
+int get_button_status();
+
+void set_floor_status(int button);
+
+void set_button_lamp(int lamp);
+
+void run_motor(int motor_dir);
+
+void stop_motor(int current_dir);
