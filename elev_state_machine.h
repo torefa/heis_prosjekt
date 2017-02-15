c8575364dcb88d462bd53ad0c6e2003bec1e97bb
torefa <torfad@gmail.com>
2017-02-15 19:01:22 +0100

    Update elev_state_machine.h

--- elev_state_machine.h
+++ elev_state_machine.h
@@ -1,1 +1,16 @@
+// The state machine for the elevator
 
+#define state
+
+
+// Event button is pressed
+int evButtonPressed(){}
+
+// Event floor is reached
+int evFloor_reached(){}
+
+// Event timer has timed out
+int evTime_out(){}
+
+// Event Stop button is pressed
+int evStop_button_signal(){}
