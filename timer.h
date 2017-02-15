19a14561b170f8e6302a566a96e1be0f1cb9f03c
torefa <torfad@gmail.com>
2017-02-15 18:45:44 +0100

    Update timer.h

--- timer.h
+++ timer.h
@@ -4,12 +4,12 @@
 #include <unistd.h>
 
 // Function to start the timer.
-void timer_start(){
+int timer_start(){
   sleep(3);
-  time_out(0);
+  return 0;
 }
 
 // Funtion to notify that the time is out
-int time_out(int time){
-  return time;
+int time_out(){
+ 
 }
