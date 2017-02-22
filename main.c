e810429015bb80fbc0d50adb9cd93fc52e44a631
Magnus Danielsen <magnudan@stud.ntnu.no>
2017-02-22 16:39:47 +0100

    Main-oppdatering
    Initialisering og enkel motorkjøring

--- main.c
+++ main.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
-#include "hw.h"
+#include "elev.h"
 #include "timer.h"
 #include "elev_state_machine.h"
+#include "queue.h"
 
 
 int main() {
+      // Initialize hardware
+    if (!elev_init()) {
+        printf("Unable to initialize elevator hardware!\n");
+        return 1;
+    }
+
+    printf("Press STOP button to stop elevator and exit program.\n");
+
+	int current_dir = DIRN_UP;
+	
+    elev_set_motor_direction(current_dir);
+
+    while (1) {
+        // Change direction when we reach top/bottom floor
+        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
+            elev_set_motor_direction(DIRN_DOWN);
+			current_dir = DIRN_DOWN;
+        } else if (elev_get_floor_sensor_signal() == 0) {
+            elev_set_motor_direction(DIRN_UP);
+			current_dir = DIRN_UP;
+        }
+
+        // Stop elevator and exit program if the stop button is pressed
+        if (elev_get_stop_signal()) {
+            elev_stop_motor(current_dir);
+            break;
+        }
+    }
+
+    return 0;
   
   
   return 0;
