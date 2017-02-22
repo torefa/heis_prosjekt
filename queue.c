de613004a462a89dae8396e25507a61832e97f14
Magnus Danielsen <magnudan@stud.ntnu.no>
2017-02-22 19:50:11 +0100

    Queue update
    Full (current) functionality implemented to queue module

--- queue.c
+++ queue.c
@@ -1,1 +1,56 @@
 
+static int up_queue[4] = {0};
+static int down_queue[4] = {0};
+
+void set_queue(int order, int pos){
+	
+	if(order == pos){
+		up_queue[order] == down_queue[order] = 1;
+	}
+	else if ((order > pos){
+		up_queue[order] = 1;
+	}
+	else if ((order > pos){
+		down_queue[order] = 1;
+	}
+	
+}
+
+void set_up_queue(int order){
+	up_queue[order] = 1;
+}
+
+void set_down_queue(int order){
+	down_queue[order] = 1;
+}
+
+void delete_floor(int floor){
+	up_queue[floor] = down_queue[floor] = 0;
+}
+
+void delete_queue(void){
+	int i;
+	for (i =0; i < 4; i++){
+		delete_floor(i);
+	}
+}
+
+int get_queue(int floor, int current_dir){
+	int i;
+	for (i = floor; (i > -1) && (i < 4); i + current_dir){
+		if(check_floor(i, current_dir) == 1){
+			return 1;
+			break;
+		}
+	}
+	return -1;
+}
+
+int check_floor(int floor, int current_dir){
+		if (current_dir > 0){
+		return up_queue[floor];
+	}
+	else if (current_dir < 0){
+		return down_queue[floor];
+	}
+}
--- queue.h
+++ queue.h
@@ -1,7 +1,38 @@
 // Header for the queue file
 
-// Add new order and return the queue
-int set_queue(int order, int pos, int dir);
+/* 
+Adds floor order to up or down queue based on elevators relative position
+*/
+void set_queue(int order, int pos);
 
-// Remove reached floor and return queue
-int update_queue(int floor);
+
+/*
+Adds floor order to up queue
+*/
+void set_up_queue(int order);
+
+/*
+Adds floor order to down queue
+*/
+void set_down_queue(int order);
+
+/*
+Deletes floor order from both queues
+*/
+void delete_floor(int floor);
+
+/*
+Deletes all floor orders from both queues
+*/
+void delete_queue(void);
+
+/*
+Checks all floors orders in the direction the elevator is/was moving, and returns true if there is a floor order.
+If none of the floors in the given direction are orderd, the function returns -1.
+*/
+int get_queue(int floor, int current_dir);
+
+/*
+Chechs queue for given direction and floor. Returns true in there is an order for given floor in respective queue.
+*/
+int check_floor(int floor, int current_dir);
