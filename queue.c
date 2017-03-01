cd49e6c738de932e2af3b969fa57a3d13c17af6c
torefa <torfad@gmail.com>
2017-03-01 18:23:40 +0100

    Update queue.c

--- queue.c
+++ queue.c
@@ -5,27 +5,27 @@ static int down_queue[4] = {0};
 void queue_set_queue(int order, int pos){
 	
 	if(order == pos){
-		queue_up_queue[order] == down_queue[order] = 1;
+		up_queue[order] == down_queue[order] = 1;
 	}
 	else if ((order > pos){
-		queue_up_queue[order] = 1;
+		up_queue[order] = 1;
 	}
 	else if ((order > pos){
-		queue_down_queue[order] = 1;
+		down_queue[order] = 1;
 	}
 	
 }
 
 void queue_set_up_queue(int order){
-	queue_up_queue[order] = 1;
+	up_queue[order] = 1;
 }
 
 void queue_set_down_queue(int order){
-	queue_down_queue[order] = 1;
+	down_queue[order] = 1;
 }
 
 void queue_delete_floor(int floor){
-	queue_up_queue[floor] = queue_down_queue[floor] = 0;
+	up_queue[floor] = down_queue[floor] = 0;
 }
 
 void queue_delete_queue(void){
@@ -48,9 +48,9 @@ int queue_get_queue(int floor, int motor_dir){
 
 int queue_check_floor(int floor, int motor_dir){
 		if (motor_dir > 0){
-		return queue_up_queue[floor];
+		return up_queue[floor];
 	}
 	else if (motor_dir < 0){
-		return queue_down_queue[floor];
+		return down_queue[floor];
 	}
 }
