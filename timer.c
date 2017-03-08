6f259aca11618128cd8f2c57fc47f6a83a896c41
Magnus Danielsen <magnudan@stud.ntnu.no>
2017-03-08 15:18:04 +0100

    Timer-update
    Lagt til en variabel som sjekker om verdien på time_out har endret seg
    siden forrige polling.

--- timer.c
+++ timer.c
@@ -3,10 +3,14 @@
 #include <time.h>
 
 // Indication of running timer
-static  int time_out = 0;
+static int time_out = 0;
+// Variable to indicate if the timer has been read or not
+// Exists to not trig event evTimoout for each poll
+static int time_read = 0;
 
 // 3 second timer
 void timer_start(){
+	time_read = 0;
 	time_out = 0;
 	float start_time = clock();
 }
@@ -17,8 +21,9 @@ int timer_time_out(){
 	if(delta_time < 3){
 		time_out = 0;
 	}
-	else{
+	else if(delta_time > 3 && time_read == 0){
 		time_out = 1:
+		time_read = 1;
 	}
 	return timer_active;
 }
