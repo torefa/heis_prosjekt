5e7e317054553d3551c103326136a11cdfa040bb
torefa <torfad@gmail.com>
2017-03-08 23:26:35 +0100

    FSM-update

--- elev_state_machine.c
+++ elev_state_machine.c
@@ -1,4 +1,4 @@
-// The elevator stat machine
+// The elevator state machine
 
 #include "elev_state_machine.h"
 
@@ -32,7 +32,7 @@ void evInitialize(){
     elev_set_motor_direction(motor_dir);
 	while(1){
 		if(elev_get_floor_sensor_signal() != -1){
-			elev_set_motor_direction(0);
+			elev_set_motor_direction(DIRN_STOP);
 			current_floor = elev_get_floor_sensor_signal();
 			break;
 		}
@@ -41,14 +41,16 @@ void evInitialize(){
 }
 
 void evFloor_reached(int floor){
-	printf("evFloor_reached: %d\n, motor dir = %d\n",floor,motor_dir);
+	printf("evFloor_reached: %d, motor dir = %d\n",floor,motor_dir);
 	elev_set_floor_indicator(floor);
 	current_floor = floor;
-	
+	printf("Er det en bestilling her? %d\n", queue_check_floor(current_floor, motor_dir));
 	if (queue_check_floor(current_floor, motor_dir)){
-			elev_set_motor_direction(0);
+			elev_set_motor_direction(DIRN_STOP);
 			timer_start();
+			printf("HA!\n");
 			elev_set_door_open_lamp(1);
+			printf("Nei?\n");
 		
 			//Turn off button lamps for the floor
 			elev_turn_off_button_lamp(current_floor);
@@ -92,33 +94,63 @@ void evButton_pressed(elev_button_type_t button, int floor){
 	//Check button type and set the relevant queue.
 	if (button == BUTTON_CALL_UP && floor != 3){
 		queue_set_up_queue(floor);
+		printf("Satt opp queue!\n");
 	} else if (button == BUTTON_CALL_DOWN && floor != 0){
-		queue_set_down_queue(floor);		
+		queue_set_down_queue(floor);
+		printf("Satt ned queue!\n");		
 	} else if (button == BUTTON_COMMAND){		
 		queue_set_queue(floor, current_floor);
+		printf("Satt begge queues!\n");
 	}
 	
 	//Start elevator in direction of button order if not already moving.
 	switch(el_state){
 	case S_STOPBUTTON:
-		break;
+			if (queue_get_queue(floor, motor_dir)){
+			elev_set_motor_direction(motor_dir);			
+			el_state = S_MOVING;
+			break;
+		}else if (queue_get_queue(floor, -motor_dir)){
+			motor_dir = -motor_dir;
+			elev_set_motor_direction(motor_dir);
+			el_state = S_MOVING;
+			break;
+		}else {
+			el_state = S_IDLE;
+			break;
+		}
+		
 	case S_IDLE:
+		printf("CASE: IDLE\n");
 		if (floor - current_floor > 0){
-			elev_set_motor_direction(1);
-			if(button == BUTTON_CALL_DOWN){
-				motor_dir = DIRN_DOWN;
-			}
-		} else {
-			elev_set_motor_direction(-1);
-			if(button == BUTTON_CALL_UP){
+		printf("Opp\n");
+			elev_set_motor_direction(DIRN_UP);
 				motor_dir = DIRN_UP;
-			}
+
+		} else {
+		printf("Ned\n");
+			elev_set_motor_direction(DIRN_DOWN);
+				motor_dir = DIRN_DOWN;
 		}
 		break;
+		
 	case S_STOPBUTTON_AT_FLOOR:
-		break;
+		if (queue_get_queue(floor, motor_dir)){
+			elev_set_motor_direction(motor_dir);			
+			el_state = S_MOVING;
+			break;
+		}else if (queue_get_queue(floor, -motor_dir)){
+			motor_dir = -motor_dir;
+			elev_set_motor_direction(motor_dir);
+			el_state = S_MOVING;
+			break;
+		}else {
+			el_state = S_IDLE;
+			break;
+		}
 	case S_MOVING:
 		break;
+		
 	default:
 		if (queue_get_queue(floor, motor_dir)){
 			elev_set_motor_direction(motor_dir);			
@@ -156,23 +188,18 @@ void evTime_out(){
 
 
 //Event for activated stop button
-void evStop_button_signal(int active){
-	if (active){
-		printf("evStop_button_signal\n");
-		// Stop motor, light stop button and erase queue.
-			elev_set_motor_direction(0);
-			elev_set_stop_lamp(1);
-			queue_delete_queue();
-			elev_clear_all_button_lamps();
-			if (el_state == S_AT_FLOOR){
-				elev_set_door_open_lamp(1);
-				el_state = S_STOPBUTTON_AT_FLOOR;
-			}else {		
-				el_state = S_STOPBUTTON;
-			}		
-			
-	}else {
-		elev_set_stop_lamp(0);
-	}
-	
+void evStop_button_signal(){
+	printf("evStop_button_signal\n");
+	// Stop motor, light stop button and erase queue.
+		elev_set_motor_direction(DIRN_STOP);
+		elev_set_stop_lamp(1);
+		queue_delete_queue();
+		elev_clear_all_button_lamps();
+		if (el_state == S_AT_FLOOR){
+			elev_set_door_open_lamp(1);
+			el_state = S_STOPBUTTON_AT_FLOOR;
+		}else {		
+			el_state = S_STOPBUTTON;
+		}		
+
 }
