65fecf3112266c1728bbc9ebae2ae5cd439129c6
Magnus Danielsen <magnudan@stud.ntnu.no>
2017-03-08 15:25:48 +0100

    Timer-comments
    Lagt til (forhåpentlig vis) meningsfulle kommentarer til timer.c

--- timer.c
+++ timer.c
@@ -8,16 +8,21 @@ static int time_out = 0;
 // Exists to not trig event evTimoout for each poll
 static int time_read = 0;
 
-// 3 second timer
+/* 
+	Function resets variables time_read and time_out to default values and sets start_time to current time.
+*/
 void timer_start(){
 	time_read = 0;
 	time_out = 0;
 	float start_time = clock();
 }
 
-// Returns the status of the timer
+/*
+	Function returns the timer status
+	Function only returns 1 if the timer has been re-started and 3 seconds has elapsed since last poll.
+*/ 
 int timer_time_out(){
-	float delta_time = (float)(clock() - start_time) / CLOCKS_PER_SEC;
+	float delta_time = (float)(clock() - start_time) / CLOCKS_PER_SEC; //Time since timer_start() converted to seconds.
 	if(delta_time < 3){
 		time_out = 0;
 	}
