85a811864fd9e0c333fad8f252e1f23f4ef74e9a
Magnus Danielsen <magnudan@stud.ntnu.no>
2017-03-08 19:58:59 +0100

    Queue-update
    Har lagt alle else og else if på samme linje som } i queue.c

--- queue.c
+++ queue.c
@@ -7,11 +7,9 @@ static int down_queue[4] = {0};
 void queue_set_queue(int order, int pos){
 	if(order == pos){
 		up_queue[order] = down_queue[order] = 1;
-	}
-	else if (order > pos){
+	} else if (order > pos){
 		up_queue[order] = 1;
-	}
-	else if (order > pos){
+	} else if (order > pos){
 		down_queue[order] = 1;
 	}
 	
@@ -50,11 +48,9 @@ int queue_get_queue(int floor, int motor_dir){
 int queue_check_floor(int floor, int motor_dir){
 		if (motor_dir > 0){
 		return up_queue[floor];
-	}
-	else if (motor_dir < 0){
+	} else if (motor_dir < 0){
 		return down_queue[floor];
-	}
-   else { // Should not happen, as motor_dir is either 1 or -1
+	} else { // Should not happen, as motor_dir is either 1 or -1
 	   return -1;
    }
 }
