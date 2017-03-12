bb35e62147430c21733c60a6405b3a465e8e2fa4
Magnus Danielsen <magnudan@stud.ntnu.no>
2017-03-12 19:04:03 +0100

    Litt til

--- elev.h
+++ elev.h
@@ -6,29 +6,11 @@
 #define __INCLUDE_DRIVER_H__
 
 
-// Number of floors
-#define N_FLOORS 4
-
-// Number of button types
-#define N_BUTTONS 3
-
-
 /**
   Initialize elevator.
   @return Non-zero on success, 0 on failure.
 */
 int elev_init(void);
-
-
-
-/**
-  Motor direction for function elev_set_motor_direction().
-*/
-typedef enum tag_elev_motor_direction { 
-    DIRN_DOWN = -1,
-    DIRN_STOP = 0,
-    DIRN_UP = 1
-} elev_motor_direction_t;
 
 
 
@@ -85,18 +67,6 @@ int elev_get_floor_sensor_signal(void);
   @param floor Which floor lamp to turn on. Other floor lamps are turned off.
 */
 void elev_set_floor_indicator(int floor);
-
-
-
-/**
-  Button types for function elev_set_button_lamp() and elev_get_button().
-*/
-typedef enum tag_elev_lamp_type { 
-    BUTTON_CALL_UP = 0,
-    BUTTON_CALL_DOWN = 1,
-    BUTTON_COMMAND = 2
-} elev_button_type_t;
-
 
 
 /**
